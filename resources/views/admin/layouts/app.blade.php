26e0c638a69c75312c0d4b36aa668dd665a34159
abdu hawi <ahhh42@gmail.com>
2021-07-23 15:35:56 +0300

    start menu

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,6 @@
+@include('admin.layouts.head')
+@include('admin.layouts.nav')
+@include('admin.layouts.sidebar')
+@yield('content')
+@include('admin.layouts.sidebarControl')
+@include('admin.layouts.footer')
